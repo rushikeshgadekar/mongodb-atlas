850d52ff3b3c06b84b3513cb8b466687bf6e834b
rushikeshgadekar <rushikeshgadekar17399@gmail.com>
2021-03-22 01:54:12 +0530

    final db update

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require("express");
 const connectDB = require("./db");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+
 app.use(cors());
 app.use(express.json({ extended: false }));
 
 connectDB();
-const port = 5000;
 
 var ur = require("./routes/user");
 
 app.use("/user", ur);
 
-app.listen(port, () => {
-  console.log("running on port:" + port);
+app.listen(process.env.PORT, () => {
+  console.log("running on port:" + process.env.PORT);
 });
